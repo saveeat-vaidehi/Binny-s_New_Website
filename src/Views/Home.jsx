d38b6039ae4b1fb7ff991431e6d5285e6f156340
chetanbnagmotisaveeat <chetan.nagmoti@saveeat.in>
2024-04-03 17:24:36 +0530

    Aboutremove

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
    
     <Container fluid className='p-0 m-0' >
       <Video_Frame />
-      <About_Frame />
+      {/* <About_Frame /> */}
       <Experties_Frame />
       <Card_Frame />
       <Gallary_Frame />
